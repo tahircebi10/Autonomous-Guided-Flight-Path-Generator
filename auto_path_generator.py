d5813f54ae00c22e7c2aa4dcc817944ce52c7ed8
tahircebi10 <cebitahir10@gmail.com>
2024-12-29 16:23:06 +0300

    libraries and Mavlink class

---
+++ auto_path_generator.py
@@ -0,0 +1,25 @@
+
+##############################################created by miyawaki############################################################
+#library imports
+import time
+from pymavlink import mavutil
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import math
+
+#MAVLinkConnection class
+class MAVLinkConnection:
+    def __init__(self):
+        self.connection = mavutil.mavlink_connection('tcp:localhost:14550')
+        self.connection.wait_heartbeat()
+        print("MAVLink bağlantısı başarılıyla yapıldı")
+        #test için bir fonksiyon ekleyeceğim genel formatı göstermek adına alınan veriler
+    def test_connection(self):
+        for i in range(3): # 3 kere veri alıyoruz
+            msg = self.connection.recv_match(blocking=True)
+            print(f"Veri {i+1}: {msg.get_type()} | {msg.to_dict()}")
+
+
+if __name__ == "__main__":
+    main()
